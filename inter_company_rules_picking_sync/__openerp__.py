5703a757c2b4e1b6a5c957ff8b1a40086418bd30
eLBati <lorenzo.battistini@agilebg.com>
2016-08-05 17:35:33 +0200

    ADD inter_company_rules_picking_sync

---
+++ inter_company_rules_picking_sync/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Lorenzo Battistini - Agile Business Group
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Synchronize inter company pickings",
+    "summary": "Extension of Inter Company Module for delivery orders",
+    "version": "8.0.1.0.0",
+    "website": "https://www.agilebg.com",
+    "author": "Agile Business Group, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "inter_company_rules",
+    ],
+    "data": [
+    ],
+}
---
+++ inter_company_rules_picking_sync/models/picking.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Lorenzo Battistini - Agile Business Group
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, fields, models, _
+from openerp.exceptions import Warning as UserError
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class stock_transfer_details(models.TransientModel):
+    _inherit = 'stock.transfer_details'
+
+    @api.one
+    def do_detailed_transfer(self):
+        res = super(stock_transfer_details, self).do_detailed_transfer()
+        picking_model = self.env['stock.picking']
+        # reading as admin to read other company's data
+        picking = picking_model.sudo().browse(self.picking_id.id)
+        if picking.sale_id:
+            po = None
+            if picking.sale_id.auto_purchase_order_id:
+                po = picking.sale_id.auto_purchase_order_id
+            else:
+                pos = self.env['purchase.order'].search([
+                    ('auto_sale_order_id', '=', picking.sale_id.id)])
+                if len(pos) > 1:
+                    raise UserError(_(
+                        "Too many purchase orders found for sale order "
+                        "%s") % picking.sale_id.name)
+                if pos:
+                    po = pos[0]
+            if po:
+                if len(po.picking_ids) > 1:
+                    _logger.info(
+                        "Too many picking for purchase order %s. Skipping"
+                        % po.name)
+                elif len(po.picking_ids) == 1:
+                    other_picking = po.picking_ids[0]
+                    other_user_id = other_picking.create_uid.id
+                    other_picking = picking_model.sudo(
+                        other_user_id
+                    ).browse(other_picking.id)
+                    wizard_id = other_picking.do_enter_transfer_details()[
+                        'res_id']
+                    wizard = self.env['stock.transfer_details'].sudo(
+                        other_user_id).browse(wizard_id)
+                    wizard.item_ids.unlink()
+                    line_model = self.env['stock.transfer_details_items']
+                    sourceloc_id = other_picking.move_lines[0].location_id.id
+                    destinationloc_id = other_picking.move_lines[
+                        0].location_dest_id.id
+                    for line in self.item_ids:
+                        line_model.sudo(other_user_id).create({
+                            'transfer_id': wizard_id,
+                            'product_id': line.product_id.id,
+                            'product_uom_id': line.product_uom_id.id,
+                            'quantity': line.quantity,
+                            'lot_id': line.lot_id and line.lot_id.id or None,
+                            'sourceloc_id': sourceloc_id,
+                            'destinationloc_id': destinationloc_id,
+                        })
+                    wizard.do_detailed_transfer()
+        return res
